e6be3a24adbacc3727a184f75bc748005f4b79fe
Eric <evandenoutenaar@gmail.com>
2020-10-01 16:55:00 +0200

    added basics for hero

--- portfolio-frontend/src/components/Hero.js
+++ portfolio-frontend/src/components/Hero.js
@@ -5,8 +5,90 @@ import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
 
-const Hero = () => {
-  return <h2>hero component</h2>
-}
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
+const Hero = () => {
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
+  // console.log(data)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <h1>I'm Eric</h1>
+            <div className="underline"></div>
+            <h4>Studying to become a software developer</h4>
+            - 🔭 I’m currently working on my own Portfolio site
+            <br />
+            - 🌱 I am also currently working on React app at Cybercom
+            <br />
+            - 🤔 I would love to work within frontend
+            <br />
+            - 💬 I have worked for many years as a journalist
+            <br />
+            - 😄 Decided to change careers to become software developer <br />-
+            💜{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://strapi.io/documentation/v3.x/getting-started/introduction.html"
+            >
+              Strapi
+            </a>
+            ,{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://reactjs.org/docs/getting-started.html"
+            >
+              React
+            </a>
+            ,{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.gatsbyjs.org/docs/"
+            >
+              Gatsby
+            </a>{" "}
+            ,{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://styled-components.com/docs"
+            >
+              Styled Components
+            </a>
+            <br />
+            - ⚡ Fun fact: I got to interview Elon Musk
+            <br />
+            - 📫 How to reach me: use the link below :)
+            <br />
+            <Link to="/contact" className="btn">
+              kontakta mig
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
+}
 export default Hero
