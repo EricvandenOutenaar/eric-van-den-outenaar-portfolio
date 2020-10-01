2a8468d57bb8ea3ef4202bf6ab7f8f9678effc6a
Eric <evandenoutenaar@gmail.com>
2020-10-01 16:43:41 +0200

    added basis layout and navbar

--- portfolio-frontend/src/components/Layout.js
+++ portfolio-frontend/src/components/Layout.js
@@ -1,12 +1,15 @@
 import React from "react"
 // import "../css/main.css"
 import Navbar from "./Navbar"
-import Sidebar from "./Sidebar"
+// import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 const Layout = ({ children }) => {
   return (
     <>
-      <h2>layout component</h2>
+      <Navbar />
+
+      {children}
+      <Footer />
     </>
   )
 }
--- portfolio-frontend/src/pages/index.js
+++ portfolio-frontend/src/pages/index.js
@@ -1,12 +1,36 @@
 import React from "react"
-import { graphql } from "gatsby"
+// import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
-import Blogs from "../components/Blogs"
+// import Blogs from "../components/Blogs"
 export default () => {
-  return <h2>index page</h2>
+  return <Layout></Layout>
 }
-// ...GatsbyImageSharpFluid
+/*
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+  */
