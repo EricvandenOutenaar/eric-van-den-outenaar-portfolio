a5c89eaaa2c3c10455eea1ff8fc1e11189e2ec98
Eric <evandenoutenaar@gmail.com>
2020-10-02 14:38:29 +0200

    added project table strapi and content

--- portfolio-frontend/src/pages/index.js
+++ portfolio-frontend/src/pages/index.js
@@ -1,21 +1,25 @@
 import React from "react"
-// import { graphql } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
-// import Blogs from "../components/Blogs"
-export default () => {
+import Blogs from "../components/Blogs"
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} title="projekt i fokus" showLink />
     </Layout>
   )
 }
-/*
+
 export const query = graphql`
   {
     allStrapiProjects(filter: { featured: { eq: true } }) {
@@ -39,4 +43,4 @@ export const query = graphql`
       }
     }
   }
-  */
+`
